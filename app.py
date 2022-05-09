1f5c4ad17d250880e52a4a844b229a7a314e6126
Joshua Cidron <jcidron09@gmail.com>
2022-05-08 22:53:40 -0400

    Starting Posts

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash, send_from_directory
-from datetime import timedelta
+from datetime import timedelta, datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import random
 import os
 import re
+import shutil
 
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 app.secret_key = "hello"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Posts-Users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -34,28 +34,26 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(1000))
+    caption = db.Column(db.String(255))
     date = db.Column(db.String(20))
     timestamp = db.Column(db.String(20))
-    poster_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    poster = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 @app.route("/")
 def home():
     content = "Test"
-    # getting files
-    os.chdir(os.getcwd() + "/static")
-    file_list = os.listdir()
+    file_list = os.listdir("static/Archives")
     for file in file_list:
-        if re.search("(([^.]+)$)", file).group() not in "txtpngjpgjpeggif'":
+        if re.search("(([^.]+)$)", file).group() not in "png jpg jpeg":
             file_list.remove(file)
-    image = random.choice(file_list)
-    os.chdir(os.getcwd()[:os.getcwd().index("/static")])
+    image = "Archives/" + random.choice(file_list)
+    print(image)
     return render_template("index.html", content=content, image=image)
 
 
 @app.route("/create_user", methods=["GET", "POST"])
 def create_user():
-    content = ""
     if request.method == 'POST':
         username = request.form["username"]
         alias = request.form["alias"]
@@ -88,32 +86,12 @@ def login():
                 session["user"] = username
                 session["alias"] = user.alias
                 return redirect(url_for("profile_screen", username=username))
-        return "invalid password"
+        return render_template("login.html", content="Invalid Password")
     return render_template("login.html")
-
-
-@app.route("/database", methods=["POST", "GET"])
-def show_database():
-    if request.method == 'POST':
-        for user in User.query.all():
-            username = user.username
-            for tag in request.form.getlist(user.username):
-                if 'delete_user' in request.form.getlist(user.username):
-                    db.session.delete(user)
-                if 'make_admin' in request.form.getlist(user.username):
-                    current_user = User.query.filter_by(username=username).first()
-                    current_user.admin = True
-                if 'remove_admin' in request.form.getlist(user.username):
-                    current_user = User.query.filter_by(username=username).first()
-                    current_user.admin = False
-        db.session.commit()
-    return render_template("show_database.html", content=create_table())
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile_screen(username):
-    username = ""
-    alias = ""
     if request.method == "POST":
         session.pop("user", None)
         return redirect(url_for("login"))
@@ -128,40 +106,64 @@ def profile_screen(username):
                     return render_template("profile.html", alias=alias, username=username)
 
 
+@app.route("/archives", methods=['GET', 'POST'])
+def archives():
+    return render_template("archives.html", gallery=create_gallery("Archives"))
+
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# @app.route('/uploads/<name>')
-# def download_file(name):
-#    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route("/create_post", methods=['POST', 'GET'])
+def create_post():
+    if 'user' not in session.keys():
+        return redirect(url_for("login"))
+    else:
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                return redirect(request.url)
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return render_template("create_post.html", content="POST")
+        if request.method == 'GET':
+            return render_template("create_post.html", content="GET")
 
 
-@app.route("/archives", methods=['GET', 'POST'])
-def archives():
+@app.route("/database", methods=["POST", "GET"])
+def show_database():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return render_template("archives.html", gallery=create_gallery("Archives"))
+        for user in User.query.all():
+            username = user.username
+            if 'delete_user' in request.form.getlist(user.username):
+                db.session.delete(user)
+            if 'make_admin' in request.form.getlist(user.username):
+                current_user = User.query.filter_by(username=username).first()
+                current_user.admin = True
+            if 'remove_admin' in request.form.getlist(user.username):
+                current_user = User.query.filter_by(username=username).first()
+                current_user.admin = False
+        db.session.commit()
+    return render_template("show_database.html", user_table=create_user_table(), post_table=create_post_table())
 
 
 def create_gallery(directory):
     gallery = "\t\t\t<div class=\"row\">\n"
     file_list = os.listdir("static/Archives")
     for file in file_list:
-        if re.search("(([^.]+)$)", file).group() not in "jpgjpegpnggifhiec":
+        if re.search("(([^.]+)$)", file).group() not in "jpg jpeg png gif heic":
             file_list.remove(file)
     for image in file_list:
         gallery += "\t\t\t\t<div class=\"col-lg-4 mb-4 mb-lg-0\">\n"
@@ -174,7 +176,8 @@ def create_gallery(directory):
     gallery += "\t\t\t</div>"
     return gallery
 
-def create_table():
+
+def create_user_table():
     table = """
     <table class="table">
         <thead>
@@ -214,6 +217,32 @@ def create_table():
             + "<label class=""form-check-label"">" + "Delete User" \
             + "</label>\n</div>\n</td>\n"
         row += 1
+    table += "</tbody>\n</table>\n"
+    return table
+
+
+def create_post_table():
+    table = """
+    <table class="table">
+        <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Image</th>
+              <th scope="col">Date</th>
+              <th scope="col">Timestamp</th>
+              <th scope="col">Event</th>
+              <th scope="col">Poster</th>
+            </tr>
+        </thead>
+    """
+    for post in Post.query.all():
+        table += "<tr>\n" \
+            + "<th scope=""row"">" + str(post.id) + "</td>\n" \
+            + "<td>" + post.image + "</td>\n" \
+            + "<td>" + post.date + "</td\n>" \
+            + "<td>" + post.timestamp + "</td>\n" \
+            + "<td>" + post.event + "</td>\n" \
+            + "<td>" + post.poster_id + "</td>\n"
     table += "</tbody>\n</table>\n"
     return table
 
